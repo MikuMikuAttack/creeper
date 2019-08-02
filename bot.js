6bbfbb05ce398cdf6a897f7817a0054a4f74c8f5
MikuMikuAttack <53520916+MikuMikuAttack@users.noreply.github.com>
2019-08-02 11:20:33 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,15 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
 
- 
-
 client.on('ready', () => {
-
     console.log('I am ready!');
-
 });
-
- 
 
 client.on('message', message => {
-
     if (message.content === 'creeper') {
-
-       message.reply('aw man');
-
-       }
-
+    	message.reply('aw man');
+  	}
 });
 
- 
-
 // THIS  MUST  BE  THIS  WAY
-
-client.login(process.env.NjA2MDg0NTE1NzQ3NTk0Mjcx.XUO2TA.gwQmS4aE2fGXE1dMUo5uVpP2Fzk);//where BOT_TOKEN is the token of our bot
+client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
