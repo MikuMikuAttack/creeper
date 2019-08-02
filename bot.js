27c454c0dc446a0ca2ac56b3ffa9c37d935f9636
MikuMikuAttack <53520916+MikuMikuAttack@users.noreply.github.com>
2019-08-02 12:07:11 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -71,5 +71,11 @@ client.on('message', message => {
   	}
 });
 
+client.on('message', message => {
+    if (message.content === 'hi') {
+    	message.reply('hi!');
+  	}
+});
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);//where BOT_TOKEN is the token of our bot 
